446415dea0a657f51ee201b4f6c6f9baa465ee69
JunSung Kim <qusdndhks@naver.com>
2023-12-07 17:28:17 +0900

    채팅방 메세지 디자인

--- app/src/main/java/com/example/myapplication/ChatAdapter.java
+++ app/src/main/java/com/example/myapplication/ChatAdapter.java
@@ -1,14 +1,21 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.List;
 
@@ -16,11 +23,13 @@ public class ChatAdapter extends ArrayAdapter<ChatMessage> {
 
     private int layoutResource; // 각 채팅 메시지의 레이아웃 리소스 ID
     private List<ChatMessage> chatMessages; // 채팅 메시지 목록
+    private FirebaseUser currentUser; // Firebase 사용자
 
     public ChatAdapter(Context context, int layoutResource, List<ChatMessage> chatMessages) {
         super(context, layoutResource, chatMessages);
-        this.layoutResource = R.layout.list_item2; // 변경된 부분
+        this.layoutResource = R.layout.list_item2;
         this.chatMessages = chatMessages;
+        this.currentUser = FirebaseAuth.getInstance().getCurrentUser();
     }
 
     @NonNull
@@ -32,9 +41,26 @@ public class ChatAdapter extends ArrayAdapter<ChatMessage> {
 
         ChatMessage chatMessage = getItem(position);
 
-        if (chatMessage != null) {
+        if (currentUser != null && chatMessage != null) {
+            String currentUserEmail = currentUser.getEmail();
+            Log.d("ChatAdapter", "Current User: " + currentUserEmail);
+
             TextView senderTextView = convertView.findViewById(R.id.senderTextView);
             TextView messageTextView = convertView.findViewById(R.id.messageTextView);
+
+            // "@" 이전의 이메일 부분만 가져오기
+            String[] emailParts = currentUserEmail.split("@");
+            String currentUserDisplayName = emailParts[0];
+
+            if (currentUserDisplayName.equalsIgnoreCase(chatMessage.getSender())) {
+                // 현재 사용자가 보낸 메시지일 경우, 오른쪽에 정렬
+                Log.d("ChatAdapter", "Right alignment");
+                setAlignmentToRight(senderTextView, messageTextView);
+            } else {
+                // 다른 사용자가 보낸 메시지일 경우, 왼쪽에 정렬
+                Log.d("ChatAdapter", "Left alignment");
+                setAlignmentToLeft(senderTextView, messageTextView);
+            }
 
             // 발신자와 메시지를 각각의 텍스트뷰에 표시
             senderTextView.setText(chatMessage.getSender());
@@ -44,9 +70,44 @@ public class ChatAdapter extends ArrayAdapter<ChatMessage> {
         return convertView;
     }
 
+    private void setAlignmentToRight(TextView senderTextView, TextView messageTextView) {
+        ViewGroup.LayoutParams params = senderTextView.getLayoutParams();
+        if (params instanceof LinearLayout.LayoutParams) {
+            // LinearLayout의 경우
+            ((LinearLayout.LayoutParams) params).gravity = Gravity.END;
+        } else if (params instanceof RelativeLayout.LayoutParams) {
+            // RelativeLayout의 경우
+            ((RelativeLayout.LayoutParams) params).addRule(RelativeLayout.ALIGN_PARENT_END);
+        }
 
-    @Override
-    public ChatMessage getItem(int position) {
-        return chatMessages.get(position);
+        params = messageTextView.getLayoutParams();
+        if (params instanceof LinearLayout.LayoutParams) {
+            // LinearLayout의 경우
+            ((LinearLayout.LayoutParams) params).gravity = Gravity.END;
+        } else if (params instanceof RelativeLayout.LayoutParams) {
+            // RelativeLayout의 경우
+            ((RelativeLayout.LayoutParams) params).addRule(RelativeLayout.ALIGN_PARENT_END);
+        }
+    }
+
+    private void setAlignmentToLeft(TextView senderTextView, TextView messageTextView) {
+        ViewGroup.LayoutParams params = senderTextView.getLayoutParams();
+        if (params instanceof LinearLayout.LayoutParams) {
+            // LinearLayout의 경우
+            ((LinearLayout.LayoutParams) params).gravity = Gravity.START;
+        } else if (params instanceof RelativeLayout.LayoutParams) {
+            // RelativeLayout의 경우
+            ((RelativeLayout.LayoutParams) params).addRule(RelativeLayout.ALIGN_PARENT_START);
+        }
+
+        params = messageTextView.getLayoutParams();
+        if (params instanceof LinearLayout.LayoutParams) {
+            // LinearLayout의 경우
+            ((LinearLayout.LayoutParams) params).gravity = Gravity.START;
+        } else if (params instanceof RelativeLayout.LayoutParams) {
+            // RelativeLayout의 경우
+            ((RelativeLayout.LayoutParams) params).addRule(RelativeLayout.ALIGN_PARENT_START);
+        }
     }
 }
+
